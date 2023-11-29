26c4c67429b22b59ea3278154a31d604992d6b28
swatishinde09 <148969831+swatishinde09@users.noreply.github.com>
2023-11-29 16:12:48 +0530

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JenkinPipe_ConsoleApp3
+{
+    internal class Program
+    {
+        void display()
+        {
+            Console.WriteLine("Hello Pipeline");
+        }
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.display();
+            Console.ReadLine();
+        }
+    }
+}
